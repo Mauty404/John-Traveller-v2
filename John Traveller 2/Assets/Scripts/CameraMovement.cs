01388e116dfd9f105b1db624f3f81bad09d385a9
TheCoconut404 <k.borowski404@gmail.com>
2020-08-26 14:04:56 +0200

    up
    - added prefabs
    - added global script to enemies AI and rotate towards player

---
+++ John Traveller 2/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    Transform target;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+
+    void Update()
+    {
+
+    }
+}
---
+++ John Traveller 2/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform target;
+    [SerializeField]
+    float chaseRadius;
+    public float attackRadius;
+    float moveSpeed;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        moveSpeed = 5;
+    }
+
+    
+    void Update()
+    {
+        if (Vector3.Distance(target.position, transform.position) <= chaseRadius && Vector3.Distance(target.position, transform.position) > attackRadius)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+            RotateTowards();
+        }
+    }
+
+    void RotateTowards()
+    { 
+        Vector3 direction;
+        direction = target.position - transform.position;
+        direction = direction.normalized;
+        if (direction.x < 0)
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        else if (direction.x > 0)
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
+        else if (direction.x == 0)
+            transform.eulerAngles = transform.eulerAngles;
+    }
+}
