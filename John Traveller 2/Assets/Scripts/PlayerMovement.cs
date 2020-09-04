c2b361a672ceded83f2249635fdbf59aabf624e8
TheCoconut404 <k.borowski404@gmail.com>
2020-09-04 14:08:52 +0200

    character movement animations

--- John Traveller 2/Assets/Scripts/PlayerMovement.cs
+++ John Traveller 2/Assets/Scripts/PlayerMovement.cs
@@ -8,25 +8,64 @@ public class PlayerMovement : MonoBehaviour
 
     public float moveSpeed = 5f;
 
-    public Rigidbody2D rb;
+    Rigidbody2D rb;
+    Animator _animator;
 
     private Vector2 movement;
-    
+    Vector3 currentPosition;
+    Vector3 previousPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        if (IsMove())
+            _animator.SetFloat("Speed", 1f);
+        else
+            _animator.SetFloat("Speed", 0f);
+
+        if (IsAttack())
+            _animator.SetTrigger("Attack");
+
+        Rotate();
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime); 
+    }
+
+   bool IsMove()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D))
+            return true;
+        else
+            return false;
+    }
+
+    bool IsAttack()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            return true;
+        else
+            return false;
+    }
+
+    void Rotate()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
+        if (Input.GetKeyDown(KeyCode.D))
+            transform.eulerAngles = new Vector3(0f, 0f, 0f);
     }
 }
