931b7253d7d7b8fa991de482230359557dc22a2d
TheCoconut404 <k.borowski404@gmail.com>
2020-09-08 12:16:03 +0200

    added: spawner activator plate

---
+++ John Traveller 2/Assets/Scripts/SpawnerTrigger.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnerTrigger : MonoBehaviour
+{
+    EnemySpawner _enemySpawner;
+    GameObject _spawnerObj;
+    private void Start()
+    {
+        _spawnerObj = transform.parent.gameObject;
+        //_spawnerObj = GameObject.Find("Spawner");
+        _enemySpawner = _spawnerObj.GetComponent<EnemySpawner>();
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _enemySpawner.enabled = true;
+    }
+}
