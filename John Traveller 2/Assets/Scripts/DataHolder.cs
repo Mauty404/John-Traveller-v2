4ab54fa96307857176f1215f59e3bffbeb93996f
Pepesha404 <k.borowski404@gmail.com>
2020-09-08 14:42:28 +0200

    added: background music (without control)

--- John Traveller 2/Assets/Scripts/DataHolder.cs
+++ John Traveller 2/Assets/Scripts/DataHolder.cs
@@ -1,28 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class DataHolder : MonoBehaviour
 {
 
     CharacterSelector _characterSelector;
+    GameObject backgroundMusic;
     public int ID;
     public bool isMale;
 
     private void Awake()
     {
-        DontDestroyOnLoad(gameObject);
-    }
+        backgroundMusic = GameObject.Find("Background Music");
+        DontDestroyOnLoad(this.gameObject);
+        DontDestroyOnLoad(backgroundMusic.gameObject);
 
-    void Start()
-    {
-        _characterSelector = FindObjectOfType<CharacterSelector>();
     }
-
 
     void Update()
     {
-        ID = _characterSelector.ID;
-        isMale = _characterSelector.isMale;
+        if (SceneManager.GetActiveScene().buildIndex == 1)
+        {
+            _characterSelector = FindObjectOfType<CharacterSelector>();
+            ID = _characterSelector.ID;
+            isMale = _characterSelector.isMale;
+        }
+
+        
     }
+
+    
 }
