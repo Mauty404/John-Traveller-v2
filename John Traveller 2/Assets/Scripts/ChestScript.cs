21ffe07f65324fbc8cc87d885f8af37d0517b21b
m0sEdRIS <ikenilum21@gmail.com>
2020-09-10 11:17:05 +0200

    chest script

---
+++ John Traveller 2/Assets/Scripts/ChestScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestScript : MonoBehaviour
+{
+    private Transform Player;
+    private Animator animator;
+    private double distance = 2.53;
+    
+    public void OpenChest()
+    {
+        animator.SetBool("Open", true);
+    }
+    
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Player = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, Player.position) < distance)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                {
+                    OpenChest();
+                }
+            }
+        }
+
+    }
+}
