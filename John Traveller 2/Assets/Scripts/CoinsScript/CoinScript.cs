f83cef49d9cfc9f60e595faef805bb10313befb6
m0sEdRIS <ikenilum21@gmail.com>
2020-09-21 11:28:33 +0200

    added coin system

---
+++ John Traveller 2/Assets/Scripts/CoinsScript/CoinScript.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        ScoreTextScript.coinAmount += 1;
+        
+        Destroy(gameObject);
+    }
+}
