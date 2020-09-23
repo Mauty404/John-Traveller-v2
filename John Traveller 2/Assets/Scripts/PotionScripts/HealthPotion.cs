046934d67502c07e7db58e5fab77811912b27bda
m0sEdRIS <ikenilum21@gmail.com>
2020-09-23 13:53:14 +0200

    added potions

---
+++ John Traveller 2/Assets/Scripts/PotionScripts/HealthPotion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPotion : MonoBehaviour
+{
+    private PlayerMovement pm;
+    public int hpbonus;
+    private Collider2D player;
+    private Collider2D item;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").GetComponent<Collider2D>();
+        item = GetComponent<Collider2D>();
+        pm = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+    }
+
+    public void AddHP()
+    {
+        pm.hp = pm.hp + hpbonus;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (player == collider)
+        {
+            Debug.Log("Heal");
+            AddHP();
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
