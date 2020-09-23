7533dbbcda2780e35d77132d9edf35a94727ae53
Pepesha404 <k.borowski404@gmail.com>
2020-09-23 14:02:17 +0200

    chest drop

--- John Traveller 2/Assets/Scripts/ChestScripts/ChestScript.cs
+++ John Traveller 2/Assets/Scripts/ChestScripts/ChestScript.cs
@@ -8,10 +8,13 @@ public class ChestScript : MonoBehaviour
     private Animator animator;
     private double distance = 2.53;
     GameObject canvas;
+    Drop _drop;
+    bool isOpen = false;
     
     public void OpenChest()
     {
         animator.SetBool("Open", true);
+        //_drop.Dropp();
     }
     
     private void Awake()
@@ -25,19 +28,27 @@ public class ChestScript : MonoBehaviour
         Player = GameObject.FindWithTag("Player").transform;
         canvas = GameObject.Find("UI").transform.GetChild(2).gameObject;
         canvas.SetActive(false);
+        _drop = GetComponent<Drop>();
+
+        if (canvas == null)
+            Debug.Log("Attach UI gameobject");
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
         if (Vector2.Distance(transform.position, Player.position) < distance)
         {
             if (animator.GetBool("Open") == false)
                 canvas.SetActive(true);
-            if (Input.GetKeyDown(KeyCode.F))
+            if (Input.GetKeyDown(KeyCode.F) && !isOpen)
             {
                 {
                     OpenChest();
+                    _drop.Dropp();
+                    isOpen = true;
                 }
             }
         }
